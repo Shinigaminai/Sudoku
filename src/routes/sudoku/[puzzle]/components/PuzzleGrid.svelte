5add5ba66f8ff8aca590fa6ed0ca6b443be43e00
Shinigaminai <24736346+Shinigaminai@users.noreply.github.com>
2026-01-27 22:40:10 +0100

    Add mouse select and number key input

--- src/routes/sudoku/[puzzle]/components/PuzzleGrid.svelte
+++ src/routes/sudoku/[puzzle]/components/PuzzleGrid.svelte
@@ -1,15 +1,59 @@
 <script lang="ts">
-	import type { SudokuGrid } from '$lib/sudoku/types';
+	import { EmptyCellValue, type SudokuGrid } from '$lib/sudoku/types';
+
 	export let puzzleGrid: SudokuGrid;
 	export let showSolution: boolean;
+
+	// User-entered values (0 = empty)
+	let userGrid: number[][] = Array.from({ length: 9 }, () => Array(9).fill(0));
+	let selectedCell = { row: -1, column: -1 };
+
+	function handleKeydown(e: KeyboardEvent, row: number, col: number) {
+		console.log(e.code);
+
+		const cell = puzzleGrid[row][col];
+
+		if (cell.fixed) return;
+
+		if (e.key >= '1' && e.key <= '9') {
+			userGrid[row][col] = Number(e.key);
+		}
+
+		if (e.key === 'Backspace' || e.key === 'Delete' || e.key === '0') {
+			userGrid[row][col] = 0;
+		}
+	}
+
+	function isCellError(row: number, col: number) {
+		const usrValue = userGrid[row][col];
+		const correctValue = puzzleGrid[row][col].value;
+		if (usrValue === EmptyCellValue) return false;
+		return usrValue !== correctValue;
+	}
 </script>
 
 <div class="aspect-square h-full max-h-200 max-w-full contain-size">
-	<div class="sudoku-grid">
+	<div class="sudoku-grid {showSolution ? 'solved' : ''}">
 		{#each puzzleGrid as row, rowIndex}
 			{#each row as cell, colIndex}
-				<div class="cell" data-fixed={cell.fixed} data-row={rowIndex} data-col={colIndex}>
-					{cell.fixed || showSolution ? cell.value : ''}
+				<div
+					class="cell"
+					class:cell--error={isCellError(rowIndex, colIndex)}
+					role="gridcell"
+					tabindex="0"
+					onclick={(e) => {
+						(e.currentTarget as HTMLElement).focus();
+					}}
+					onkeydown={(e) => handleKeydown(e, rowIndex, colIndex)}
+					data-fixed={cell.fixed}
+					data-row={rowIndex}
+					data-col={colIndex}
+				>
+					{#if cell.fixed || showSolution}
+						{cell.value}
+					{:else if userGrid[rowIndex][colIndex] !== 0}
+						{userGrid[rowIndex][colIndex]}
+					{/if}
 				</div>
 			{/each}
 		{/each}
@@ -78,7 +122,24 @@
 		border-left: var(--block-border-width) solid var(--block-border-color);
 	}
 
+	.cell[data-fixed='false']:focus {
+		background-color: var(--cell-selected-bg);
+		box-shadow: var(--box-shadow-active);
+		z-index: 1;
+	}
+	.cell[data-fixed='true']:focus {
+		background-color: var(--disabled);
+		box-shadow: var(--box-shadow-active);
+		z-index: 1;
+	}
 	.cell[data-fixed='false']:hover {
 		background-color: var(--cell-selected-bg);
+	}
+	.cell[data-fixed='true']:hover {
+		background-color: var(--disabled);
+	}
+
+	.solved > .cell--error {
+		background-color: var(--error);
 	}
 </style>
